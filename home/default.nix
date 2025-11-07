d8712160a482e9269a2684c382ee4231fbe0aa94
gumuncle <littlefive.jp@gmail.com>
2025-11-07 13:45:50 +0900

    Fix shell.nix theme configuration and enhance prompt with random emojis

--- home/default.nix
+++ home/default.nix
@@ -6,7 +6,7 @@
   home.stateVersion = "24.05";
 
   imports = [
-    #./modules/shell.nix
+    ./modules/shell.nix
     ./modules/git.nix
     ./modules/devtools.nix
   ];
--- home/modules/shell.nix
+++ home/modules/shell.nix
@@ -3,15 +3,51 @@
     enable = true;
     oh-my-zsh = {
       enable = true;
-      theme = "robbyrussell";
+      theme = "";                # テーマによるPROMPT上書きを無効化
       plugins = [ "git" "z" "sudo" ];
     };
+
     shellAliases = {
+      ".." = "cd ..";
+      ",," = "cd ../..";
       ll = "ls -lah";
-      gs = "git status";
       py = "python3";
     };
+
+
+    initContent = ''
+      autoload -Uz vcs_info
+      precmd_vcs_info() { vcs_info }
+      precmd_functions+=( precmd_vcs_info )
+
+      GET_RANDOM_EMOJI() {
+        local emojis=("🔥" "⚡️" "🚀" "🎧" "🦅" "🐍" "💎")
+        echo ''${emojis[''$RANDOM % ''${#emojis[@]}]}
+      }
+
+      PROMPT='%F{white}┌───=[ ''${RANDOM##*}$(GET_RANDOM_EMOJI) %n::%F{white}%m%f%F{white}]-[%~]-[%D-%*] %F{white}$vcs_info_msg_0_%f
+    %F{white}└──$%f'
+    '';
+
+
+
+
   };
+
+  programs.bash = {
+    enable = true;
+    bashrcExtra = ''
+      GET_RANDOM_EMOJI() {
+        local emojis=("🔥" "⚡️" "🚀" "🎧" "🦅" "🐍" "💎")
+        echo ''${emojis[''$RANDOM % ''${#emojis[@]}]}
+      }
+      PS1="\[\e[97m\]┌───=[ \$RANDOM\$(GET_RANDOM_EMOJI) \u::\h]-[\w]-[\D{%H:%M}] \[\e[97m\]\n└──\$\[\e[0m\] "
+    '';
+  };
+
+  home.packages = with pkgs; [
+    zsh
+  ];
 
   home.sessionVariables = {
     EDITOR = "nvim";
@@ -24,4 +60,3 @@
     "/run/current-system/sw/bin"
   ];
 }
-
