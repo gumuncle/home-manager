0640a3f92c92d44c042f0f23e0a070ff3be7766b
yusuke furukawa <gumuncle@yusukenoMacBook-Air.local>
2025-12-13 20:52:16 +0900

    add

--- flake.nix
+++ flake.nix
@@ -1,50 +1,26 @@
 {
-  description = "Home Manager setup for macOS (portable)";
+    description = "My Home Manager Flake";
 
-  nixConfig = {
-    experimental-features = [ "nix-command" "flakes" ];
-    warn-dirty = true;
-  };
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+        home-manager.url = "github:nix-community/home-manager";
+        home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  outputs = { self, nixpkgs, home-manager, ... }:
-  let
-    system = "aarch64-darwin";
-    pkgs = nixpkgs.legacyPackages.${system};
-
-    # ユーザーごとのHome Manager設定を生成（純粋に定義）
-    mkHomeConfiguration = username:
-      home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [
-          ./home/default.nix
-          {
-            home.username = username;
-            home.homeDirectory = "/Users/${username}";
-          }
-        ];
-      };
-
-    homeConfigurations = {
-      cw-yusuke.furukawa = mkHomeConfiguration "cw-yusuke.furukawa";
-      gumuncle = mkHomeConfiguration "gumuncle";
+        nixvim.url = "github:nix-community/nixvim";
     };
-  in {
-    inherit homeConfigurations;
 
-    devShells.${system}.default = pkgs.mkShell {
-      buildInputs = [ pkgs.git pkgs.home-manager pkgs.nodejs_22 ];
-      shellHook = ''
-        echo "🏠 Home Manager development environment loaded!"
-        echo "👤 Current user: $USER"
-        echo "💡 Usage: nix run nixpkgs#home-manager -- switch --flake .#cw-yusuke.furukawa"
-      '';
+    outputs = { self, nixpkgs, home-manager, nixvim, ... }:
+    let
+        system = "aarch64-darwin";
+        pkgs = import nixpkgs { inherit system; };
+    in {
+        homeConfigurations.gumuncle = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = [
+                ./home.nix
+                nixvim.homeModules.nixvim
+                ./home/nixvim.nix
+            ];
+        };
     };
-  };
 }
-
--- scripts/enter.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# Nixの実験的機能を有効にしてからdevelopを実行
-# 開発シェルに入るだけ。実験機能はflakeのnixConfigで定義済み。
-nix develop --command zsh
-
